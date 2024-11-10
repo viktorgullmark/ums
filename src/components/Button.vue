a14dd186ed0961296cc5c9ebc7b5582211541192
Viktor Gullmark <viktor.gullmark@gmail.com>
2024-11-10 16:33:45 +0100

    Rework button component

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,9 +1,17 @@
 <template>
   <button
-    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-    v-text="buttonText"
+    :class="{
+      [`bg-${variant}-700`]: true,
+      [`hover:bg-${variant}-900`]: !isDisabled,
+      [`bg-opacity-50 hover:bg-opacity-50`]: isDisabled,
+    }"
+    class="flex items-center text-white py-2 px-4 rounded"
+    :disabled="isDisabled"
     @click.prevent="onClick()"
-  />
+  >
+    <slot name="leading" />
+    <span>{{ buttonText }}</span>
+  </button>
 </template>
 
 <script>
@@ -13,10 +21,28 @@ export default {
       type: Function,
       default: () => "",
     },
+    variant: {
+      type: String,
+      default: "primary",
+    },
     buttonText: {
       type: String,
       default: "Button",
     },
+    isDisabled: {
+      type: Boolean,
+      default: false,
+    },
   },
 };
 </script>
+
+<style>
+img {
+  filter: invert(100%) sepia(0%) saturate(1%) hue-rotate(266deg)
+    brightness(101%) contrast(101%);
+  width: 18px;
+  height: 18px;
+  margin-right: 5px;
+}
+</style>
