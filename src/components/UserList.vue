f605732c722e1cabdef20724956ca3b75a28bf8c
Viktor Gullmark <viktor.gullmark@gmail.com>
2024-11-10 16:34:45 +0100

    Rework rendering of user list

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,38 +1,39 @@
 <template>
-  <table class="table-auto mx-auto my-10">
-    <thead>
-      <th class="px-4 py-2" v-text="'Namn'" />
-      <th class="px-4 py-2" v-text="'Date of Birth'" />
-      <th class="px-4 py-2" v-text="'Age'" />
-      <th class="px-4 py-2" v-text="'Profession'" />
-      <th class="px-4 py-2" v-text="'Country'" />
-      <th class="px-4 py-2" v-text="'Favorite Quote'" />
-      <th class="px-4 py-2" v-text="''" />
+  <table
+    class="table-auto border mx-auto w-full text-sm text-left rtl:text-right text-gray-600"
+  >
+    <thead class="text-xs text-gray-700 uppercase bg-gray-100">
+      <th class="px-6 py-3">
+        <input type="checkbox" @click="toggleSelectAll" v-model="allSelected" />
+      </th>
+      <th class="px-6 py-3" v-text="'Name'" />
+      <th class="px-6 py-3" v-text="'Date of Birth'" />
+      <th class="px-6 py-3" v-text="'Age'" />
+      <th class="px-6 py-3" v-text="'Profession'" />
+      <th class="px-6 py-3" v-text="'Country'" />
+      <th class="px-6 py-3" v-text="'Favorite Quote'" />
     </thead>
     <tbody>
-      <tr v-for="(user, key) in users" :key="key">
-        <td
-          class="border px-4 py-2"
-          v-text="user.firstName + ' ' + user.lastName"
-        />
-        <td class="border px-4 py-2" v-text="user.birthDate" />
-        <td class="border px-4 py-2" v-text="userAge(user)" />
-        <td
-          class="border px-4 py-2"
-          v-text="getProfessionNameById(user.profession_id)"
-        />
-        <td
-          class="border px-4 py-2"
-          v-text="getCountryNameById(user.country_id)"
-        />
-        <td class="border px-4 py-2" v-text="user.quote" />
-        <td class="px-4 py-2">
-          <button-component
-            class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
-            :on-click="() => removeRow(user)"
-            :button-text="'Remove'"
+      <tr v-for="(user, key) in users" :key="key" class="bg-white border-b">
+        <td class="px-6 py-3">
+          <input
+            type="checkbox"
+            :id="user.id"
+            :name="user.id"
+            :value="user.id"
+            @change="() => $emit('selected', selected)"
+            v-model="selected"
           />
         </td>
+        <td class="px-6 py-3" v-text="user.firstName + ' ' + user.lastName" />
+        <td class="px-6 py-3" v-text="user.birthDate" />
+        <td class="px-6 py-3" v-text="userAge(user)" />
+        <td
+          class="px-6 py-3"
+          v-text="getProfessionNameById(user.profession_id)"
+        />
+        <td class="px-6 py-3" v-text="getCountryNameById(user.country_id)" />
+        <td class="px-6 py-3" v-text="user.quote" />
       </tr>
     </tbody>
   </table>
@@ -41,15 +42,13 @@
 <script>
 // import User from './User';
 import { mapState, mapActions } from "vuex";
-import ButtonComponent from "./Button.vue";
 
 export default {
   name: "UserList",
-  components: {
-    ButtonComponent,
-  },
   data() {
     return {
+      showModal: false,
+      allSelected: false,
       selected: [],
     };
   },
@@ -70,17 +69,21 @@ export default {
       return new Date().getUTCFullYear() - new Date(birthDate).getUTCFullYear();
     },
     getProfessionNameById(id) {
-      console.log("professions", this.professions);
       const profession = this.professions.find(
         (profession) => profession.value === id
       );
-      console.log("profession", profession);
       return profession ? profession.text : "";
     },
     getCountryNameById(id) {
-      console.log("countries", this.countries, id);
       const country = this.countries.find((country) => country.value === id);
       return country ? country.text : "";
+    },
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleSelectAll() {
+      this.selected = this.allSelected ? [] : this.users.map((user) => user.id);
+      this.$emit("selected", this.selected);
     },
   },
 };
